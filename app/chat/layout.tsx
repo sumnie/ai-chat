b991e7dad424d8d5486a01adc537583b6ace66b9
sumnie <summie0620@gmail.com>
2025-10-28 21:19:39 +0900

    feat: tailwind theme toggle

--- app/chat/layout.tsx
+++ app/chat/layout.tsx
@@ -1,21 +1,19 @@
 'use client'; // 서버 컴포넌트로 두고 싶다면 session gate처럼 themeWrapper를 따로 만들어야
 
 import { Header } from '@/components/chat/Header';
-import { cn } from '@/lib/utils';
 import { useUserStore } from '@/store/userStore';
+import { useEffect } from 'react';
 export default function ChatLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-    const theme = useUserStore((state) => state.theme);
+  const theme = useUserStore((state) => state.theme);
+  useEffect(() => {
+    document.documentElement.classList.toggle('dark', theme === 'dark');
+  }, [theme]);
   return (
-    <div
-      className={cn(
-        'flex justify-center items-center min-h-[100vh] max-h-[100vh] overflow-y-auto',
-        theme === 'dark' ? 'bg-gray-900' : 'bg-zinc-100'
-      )}
-    >
+    <div className="flex justify-center items-center min-h-[100vh] max-h-[100vh] overflow-y-auto bg-zinc-100 text-gray-900 dark:bg-gray-900 dark:text-gray-100 transition-colors duration-300">
       <div className="flex flex-col w-full min-h-[100vh] md:w-[768px]">
         {/* Header - (l)모델 선택, (r)테마 설정,  */}
         <Header></Header>
---
+++ tailwind.config.ts
@@ -0,0 +1,7 @@
+module.exports = {
+  darkMode: ['class'], // 기본: 'media'인데 'class'로 변경해야 수동 제어 가능
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
